b863aaf2f5c1c1a7f319fba8733408858ea849aa
JaimeGSandoval <jaime.devs@gmail.com>
2022-09-22 20:41:18 -0700

    New test values

--- server/src/sum.test.ts
+++ server/src/sum.test.ts
@@ -3,7 +3,7 @@ import sum from './sum';
 describe('Sum function', () => {
   describe('given two 5 and 2', () => {
     it('Should return the sum of 7', () => {
-      expect(sum(5, 2)).toBe(7);
+      expect(sum(5, 5)).toBe(10);
     });
   });
 });
