eb237c20343c1a3aefe1e12f92b9304b0f921c3e
JaimeGSandoval <jaime.devs@gmail.com>
2022-09-22 22:49:26 -0700

    Add postgres connection and update yml file

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import pool from './db/postgres';
 
 const app = express();
 app.use(helmet());
@@ -15,6 +16,12 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json("Looks like it's working");
+});
+
+app.get('/users', async (req, res) => {
+  const response = await pool.query('select * from users');
+  console.log(response.rows);
+  return res.status(200).json(response.rows);
 });
 
 export default app;
---
+++ server/src/db/postgres.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+import path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '..', '..', '..', '/.env') });
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  database: process.env.PG_DB,
+});
+
+export default pool;
