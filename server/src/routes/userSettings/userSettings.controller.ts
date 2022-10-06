10b7e090c50c135a78846a299fa80f58344e882c
JaimeGSandoval <jaime.devs@gmail.com>
2022-10-06 13:20:03 -0700

    Add username schema for validation

--- server/src/routes/userSettings/userSettings.controller.ts
+++ server/src/routes/userSettings/userSettings.controller.ts
@@ -3,6 +3,8 @@ import { QueryResult } from 'pg';
 import AppError from '../../utils/app-error';
 import updateUsername from '../../models/userSettings/userSettings.modal';
 import { signJWT } from '../../utils/jwt.utils';
+import { userNameTaken } from '../../models/register/register.model';
+import User from '../../types/User';
 
 const httpUpdateUsername = async (
   req: Request,
@@ -13,20 +15,30 @@ const httpUpdateUsername = async (
   const { newUsername } = req.body;
 
   try {
+    const userNameUnavailable: QueryResult = await userNameTaken(newUsername);
+
+    if (userNameUnavailable.rows.length) {
+      return next(new AppError('That user name is already taken.', 409));
+    }
+
     const userUpdated: QueryResult = await updateUsername(userId, newUsername);
 
     res.locals.user.userName = userUpdated.rows[0].user_name;
 
     const { userName, email, role } = res.locals.user;
 
-    const test = {
+    const newUserData: User = {
       userId: res.locals.user.userId,
       userName,
       email,
       role,
     };
 
-    const newAccessToken = signJWT(test, process.env.ACCESS_TOKEN_SECRET as string, 260);
+    const newAccessToken: string = signJWT(
+      newUserData,
+      process.env.ACCESS_TOKEN_SECRET as string,
+      260
+    );
 
     return res.status(200).json({
       status: 'Success',
--- server/src/routes/userSettings/userSettings.router.ts
+++ server/src/routes/userSettings/userSettings.router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import httpUpdateUsername from './userSettings.controller';
+import { updateUsernameSchema } from '../../middleware/schemas';
+import validateSchemas from '../../middleware/validate.schemas';
 
 const settingsRouter: Router = Router();
 
-settingsRouter.put('/update-username', httpUpdateUsername);
+settingsRouter.put('/update-username', updateUsernameSchema, validateSchemas, httpUpdateUsername);
 
 export default settingsRouter;
